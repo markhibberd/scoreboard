631ae9f0f03755932c18d1877a63acc0a713946a
markh <markh@6473bcd3-8704-4ce9-ad8e-72c05b524614>
2009-11-05 14:36:38 +0000

    clean up.
    git-svn-id: http://subversion.assembla.com/svn/peeble/trunk@18 6473bcd3-8704-4ce9-ad8e-72c05b524614

--- config-example.js
+++ config-example.js
@@ -40,11 +40,3 @@ status(name, url);
 image(name, url);
 
 */
-
-
-hudson = "http://hudson:10080/score/";
-geekscape = "http://boost:boost@geekscape.org/dashboard/tab/score/boost";
-all = ["boost", "fist", "hammer", "juggernaut", "scoreboard", "shampoo", "web", "penetrate", ];
-for each(x in all) build(x, hudson + x);
-build("boost@geekscape", geekscape);
-image(300000, "http://www.bom.gov.au/radar/IDR662.gif");

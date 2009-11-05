bb6c6cac19e93184a848b5e86cbc0c20719b3945
markh <markh@6473bcd3-8704-4ce9-ad8e-72c05b524614>
2009-11-05 14:38:39 +0000

    clean up.
    git-svn-id: http://subversion.assembla.com/svn/peeble/trunk@19 6473bcd3-8704-4ce9-ad8e-72c05b524614

--- src/java/alien/leapstream/scoreboard/alien/aqueduct/Aqueduct.java
+++ src/java/alien/leapstream/scoreboard/alien/aqueduct/Aqueduct.java
@@ -1,5 +1,6 @@
 package leapstream.scoreboard.alien.aqueduct;
 
+// FIX AQUEDUCT Push to boost.
 public interface Aqueduct {
     ConduitIn in();
     int threads();
--- src/java/alien/leapstream/scoreboard/alien/resilient/ResilientRunnables.java
+++ src/java/alien/leapstream/scoreboard/alien/resilient/ResilientRunnables.java
@@ -3,6 +3,7 @@ package leapstream.scoreboard.alien.resilient;
 import leapstream.scoreboard.alien.resilient.ErrorHandler;
 import leapstream.scoreboard.alien.resilient.TimeoutHandler;
 
+// FIX AQUEDUCT Push to boost.
 public interface ResilientRunnables {
     Runnable imbune(Runnable runnable, ErrorHandler errors, TimeoutHandler timeouts, int timeout);
 }

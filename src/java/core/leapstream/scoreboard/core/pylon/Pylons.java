86cb87eb8286fdc9c6be95a108838ea830dae957
markh <markh@6473bcd3-8704-4ce9-ad8e-72c05b524614>
2009-11-18 10:54:19 +0000

    lifecycle.
    git-svn-id: http://subversion.assembla.com/svn/peeble/trunk@67 6473bcd3-8704-4ce9-ad8e-72c05b524614

--- src/java/core/leapstream/scoreboard/core/pylon/Pylons.java
+++ src/java/core/leapstream/scoreboard/core/pylon/Pylons.java
@@ -2,7 +2,6 @@ package leapstream.scoreboard.core.pylon;
 
 import java.util.List;
 
-// FIX LIFECYCLE Use or lose.  
 public interface Pylons extends Iterable<Pylon> {
     void add(Pylon pylon);
     // FIX LIFECYCLE Elliminate these. Drive this data structure deeper and they should not be needed.

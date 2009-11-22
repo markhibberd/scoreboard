e818e726a5218b5846771a986c1938beb6fb404b
markh <markh@6473bcd3-8704-4ce9-ad8e-72c05b524614>
2009-11-22 10:58:22 +0000

    Configuration.
    git-svn-id: http://subversion.assembla.com/svn/peeble/trunk@74 6473bcd3-8704-4ce9-ad8e-72c05b524614

--- src/java/core/leapstream/scoreboard/core/config/exposed/Seed.java
+++ src/java/core/leapstream/scoreboard/core/config/exposed/Seed.java
@@ -1,7 +1,10 @@
 package leapstream.scoreboard.core.config.exposed;
 
+import au.net.netstorm.boost.bullet.incredibles.core.Struct;
+
 // FIX CONFIG use or lose, expose configuration as a set of functions.
-public interface Seed {
+public interface Seed extends Struct {
+    String[] _ = {"key", "doc", "applicator"};
     String key();
     String doc();
     Applicator applicator();

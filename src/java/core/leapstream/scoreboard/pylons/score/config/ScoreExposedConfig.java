979e04aef4f7616b2450b21ff395839d9180aeda
markh <markh@6473bcd3-8704-4ce9-ad8e-72c05b524614>
2009-11-22 11:38:29 +0000

    Configuration.
    git-svn-id: http://subversion.assembla.com/svn/peeble/trunk@80 6473bcd3-8704-4ce9-ad8e-72c05b524614

--- src/java/core/leapstream/scoreboard/pylons/score/config/ScoreExposedConfig.java
+++ src/java/core/leapstream/scoreboard/pylons/score/config/ScoreExposedConfig.java
@@ -6,6 +6,5 @@ import leapstream.scoreboard.core.config.exposed.ConfigRegistry;
 public class ScoreExposedConfig implements ExposedConfig {
     public void expose(ConfigRegistry registry) {
         // FIX CONFIG AAAAAAAA I am here. Wire in exposed config methods.
-
     }
 }

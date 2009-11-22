d26be42a411b7e8aed62aa72fa12c848ddfd0661
markh <markh@6473bcd3-8704-4ce9-ad8e-72c05b524614>
2009-11-22 10:58:54 +0000

    Configuration.
    git-svn-id: http://subversion.assembla.com/svn/peeble/trunk@75 6473bcd3-8704-4ce9-ad8e-72c05b524614

--- src/java/core/leapstream/scoreboard/core/config/exposed/ExposedConfigs.java
+++ src/java/core/leapstream/scoreboard/core/config/exposed/ExposedConfigs.java
@@ -1,6 +1,6 @@
 package leapstream.scoreboard.core.config.exposed;
 
 // FIX CONFIG use or lose, expose configuration as a set of functions.
-public interface Configs {
-    void configs();
+public interface ExposedConfigs {
+    void expose(ConfigRegistry registry);
 }

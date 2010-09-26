327d76c81919f6c4ad6e685a21a4948bee7823c7
markh <markh@6473bcd3-8704-4ce9-ad8e-72c05b524614>
2010-09-26 12:17:35 +0000

    Fixed typo in config-example.js
    git-svn-id: http://subversion.assembla.com/svn/peeble/trunk@88 6473bcd3-8704-4ce9-ad8e-72c05b524614

--- config/config-example.js
+++ config/config-example.js
@@ -61,7 +61,7 @@ hudson = "http://www.leapstream.com.au/products/scoreboard/samples/hudson/";
 cruise = "http://www.leapstream.com.au/products/scoreboard/samples/cruise/";
 
 build("hornet", hudson + "hudson-hornet", 20000);
-build("evil", hudson + "hudson-evil);
+build("evil", hudson + "hudson-evil");
 build("cruiser", cruise + "cruiser");
 status("status", hudson + "hudson-status");
 
